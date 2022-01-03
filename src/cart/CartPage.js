23e6c8f0d265a9f681fdb62687ccdb3983ef64f5
pindilind <linda.gustafsson@outlook.com>
2022-01-03 11:04:32 +0100

    #31 Början till cartStyling. Se över när componenten kommer till

--- src/cart/CartPage.js
+++ src/cart/CartPage.js
@@ -1,6 +1,12 @@
 import React from "react";
 
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 import HeaderInlogged from "../headers/HeaderInlogged";
+
+import "../App.scss";
+import "./CartPageStyle.scss";
 
 function CartPage() {
   return (
@@ -10,7 +16,37 @@ function CartPage() {
       <HeaderInlogged />
       <div className="wrappsAllContent">
         <div className="flexCenterAll ">
-          <h1>CartPage</h1>
+          <h1>Your Cart</h1>
+
+          <Typography className={'productDiv'} component="div">
+            RENDERA COMPONENT HÄR
+          </Typography>
+
+          <Typography className={'priceDiv'} component="div">
+
+            <Typography >
+              Total amount incl. VAT
+            </Typography>
+
+            <Typography className={'totalPrice'}>
+              122000 kr
+            </Typography>
+
+          </Typography>
+
+          <Typography className={'btnDiv'} component="div">
+
+            <Button
+              /* onClick={} */
+              size="small"
+              color="success"
+              variant="contained"
+              disableElevation>
+              Go To Payment
+            </Button>
+
+          </Typography>
+
         </div>
       </div>
 
