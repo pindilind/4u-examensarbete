569aef3f59a62dadfdaacd67893e2770df6ba02a
susanisaksson <susan.isaksson@medieinstitutet.se>
2022-01-16 19:45:06 +0100

    #99 Städat i UserHomePage och tagit bort funktionen för modalen som numer finns i ProductCardSmall

--- src/userView/UserHomePage.js
+++ src/userView/UserHomePage.js
@@ -154,10 +154,7 @@ function UserHomePage(props) {
     });
   }
 
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
+  
   return (
     <>
       <HeaderInlogged product={product} counter={counter} />
