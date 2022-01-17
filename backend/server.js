8503307df1a7733752e6af5ce1eb7824062a5d9b
susanisaksson <susan.isaksson@medieinstitutet.se>
2022-01-17 09:47:47 +0100

    #77 Förberett backend

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const port = 3005;
 
 app.use(cors());
-app.use(express.json());
+app.use('/', express.json('public'));
 
 //hämtar alla produkter
 app.get("/", (req, res) => {
@@ -14,6 +14,41 @@ app.get("/", (req, res) => {
   res.json(Object.values(productList));
   /* res.json(productList)  */
 });
+
+//hämtar ut produktkategorier
+app.get("/", (req, res) => {
+  let raw = fs.readFileSync("./database/categoryDB.json") //hämtar url till jsonfil
+  let productCategoryList = JSON.parse(raw)
+  res.json(Object.values(productCategoryList));
+  /* res.json(productCategoryList)  */
+});
+
+//hämtar ut User
+app.get("/", (req, res) => {
+  let raw = fs.readFileSync("./database/userDB.json") //hämtar url till jsonfil
+  let userList = JSON.parse(raw)
+  res.json(Object.values(userList));
+  /* res.json(productCategoryList)  */
+});
+
+//förberett för att lägga in nya produkter
+app.post('/', (req,res) => {
+  try {
+    let raw = fs.readFileSync("./database/productDB2.json")
+    let newProductInput = JSON.parse(raw)
+    newProductInput.push(req.body)
+    fs.writeFileSync("./database/productDB2.json", JSON.stringify(newProductInput))
+    res.json(true)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json(false)
+  }
+  console.log(req.body)
+})
+
+
+
+
 
 
 // Implement 500 error route
@@ -28,5 +63,5 @@ app.use(function (req, res, next) {
 });
 
 app.listen(port, () => {
-    console.log('Server listen on ${PORT}');
+    console.log('Server listen on PORT: ' + port);
 })
