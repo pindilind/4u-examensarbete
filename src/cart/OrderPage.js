49c4b453336ff23f689f8965ae9fa333f57b1bd4
pindilind <linda.gustafsson@outlook.com>
2022-02-01 17:50:58 +0100

    #STRIPE FUNGERAR! TREDJE GÅNGEN GILT

--- src/cart/OrderPage.js
+++ src/cart/OrderPage.js
@@ -15,8 +15,6 @@ function OrderPage(props) {
   const [orders, setOrders] = useState([]);
 
   console.log(orders)
-  console.log(orders.cart)
-
 
   useEffect(() => {
     async function getOrders() {
@@ -38,31 +36,25 @@ function OrderPage(props) {
     let orderArray = (Object.values(orders))
     console.log(orderArray)
 
-    return orderArray.map((orders) => {
-      let cart = JSON.parse(orders.cart);
-      console.log(orders)
+    return orderArray.map((order) => {
+      console.log(order)
+      let cart = order.cart;
+      console.log(cart)
 
       return cart.map((cart) => {
-        console.log(cart)
-        /* let cart = JSON.parse(orders.cart);
-        console.log(JSON.parse(orders.cart)); */
 
         return (
-          <>
-            <div>
-
-
-              <tr style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between"
-              }}>
-                <td>{orders.customerId}</td>
-                <td>{cart.productTitle}</td>
-                <td>{cart.date}</td>
-                <td>{cart.time}</td>
-                <td>{cart.link}</td>
-              </tr>
+    
+            <tr key={cart.productTitle} style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between"
+            }}>
+              <td>{order.customerId}</td>
+              <td>{cart.productTitle}</td>
+              <td>{cart.date}</td>
+              <td>{cart.time}</td>
+              <td>{cart.link}</td>
 
               {/* <tr>
             <td>{orders.orderDate}</td>
@@ -71,10 +63,7 @@ function OrderPage(props) {
             <td>{cart.price}</td>
             <td>{cart.quantity * cart.price}</td>
           </tr> */}
-
-            </div>
-          </>
-
+            </tr>
         );
       });
     });
@@ -88,35 +77,35 @@ function OrderPage(props) {
         <div className="flexCenterAll ">
           <h1>Dina Ordrar</h1>
 
-          <Typography className={'orderDiv'} component="div">
-            <div classes="orderTable">
-              <table >
-                <tablehead style={{
+
+          <div classes="orderTable">
+            <table >
+              <thead style={{
+                width: "100%",
+                outerHeight: "8em",
+                display: "flex",
+                justifyContent: "space-around",
+                padding: "1em",
+                alignItems: "center",
+                backgroundColor: '#75A488',
+                color: '#ffffff',
+                fontWeight: 'bold',
+
+              }}>
+
+                <tr style={{
                   width: "100%",
-                  outerHeight: "8em",
                   display: "flex",
-                  justifyContent: "space-around",
-                  padding: "1em",
-                  alignItems: "center",
-                  backgroundColor: '#75A488',
-                  color: '#ffffff',
-                  fontWeight: 'bold',
-
+                  justifyContent: "space-between"
                 }}>
+                  <th>Ordernummer </th>
+                  <th>Event titel</th>
+                  <th>Event datum</th>
+                  <th>Event starttid</th>
+                  <th>Länk till event</th>
+                </tr>
 
-                  <tr style={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "space-between"
-                  }}>
-                    <th>Ordernummer </th>
-                    <th>Event titel</th>
-                    <th>Event datum</th>
-                    <th>Event starttid</th>
-                    <th>Länk till event</th>
-                  </tr>
-
-                  {/* <tr>
+                {/* <tr>
                   <th>Orderdatum</th>
                   <th>Orderstatus</th>
                   <th>Antal</th>
@@ -124,12 +113,14 @@ function OrderPage(props) {
                   <th>Total, s:a</th>
                 </tr>  */}
 
-                </tablehead>
-                {renderOrder()}
-              </table>
-            </div>
+              </thead>
 
-          </Typography>
+              <tbody>
+                {renderOrder()}
+              </tbody>
+
+            </table>
+          </div>
 
         </div>
       </div>
