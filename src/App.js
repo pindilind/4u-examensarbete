81fef16806a06503829f4ee646b423668f8d1e14
pindilind <linda.gustafsson@outlook.com>
2022-02-01 10:38:17 +0100

    #12 Cookies på sidan KLAR

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import {
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
-import './App.scss';
-
 import CreateAccount from "./login/CreateAccount";
 import Login from "./login/Login";
 import StartPage from "./start/StartPage";
@@ -17,13 +15,18 @@ import StartPage from "./start/StartPage";
 import CartPage from "./cart/CartPage";
 
 import OrderPage from "./cart/OrderPage";
-//import OrderPageTest from "./cart/OrderPageTest";
 
 import UserHomePage from "./userView/UserHomePage";
 import UserInfo from "./userView/UserInfo";
 import SuccsessPage from "./stripe/SuccessPage";
 import CancelPage from "./stripe/CancelPage";
 import ScrollToTop from "./ScrollToTop";
+import CookieConsent, { Cookies } from "react-cookie-consent";
+
+
+/* import WithCookies from "./cookies/Cookies"; */
+
+import './App.scss';
 
 const stripePromise = loadStripe('pk_test_51KIrmMKydFVV4O5pbXcVA2jLQbS3wNlbptKM3U9V725b9pBtZNB8eaCajooBNfRl4QJ88SVIhgv61xnVZDnmY352003CBKMCVi');
 
@@ -38,9 +41,15 @@ function App() {
       <ScrollToTop />
 
       <Switch>
-
         <Route exact path="/">
           <StartPage />
+          <CookieConsent 
+          debug={true}
+          style={{ background: '#000'}}
+          buttonStyle={{ background: '#75A488', color: '#fff' }}
+          buttonText="Acceptera"
+          >
+            Denna webbplats använder cookies för att förbättra användarupplevelsen. Se vår integritetspolicy, genom att acceptera detta meddelande.</CookieConsent>
         </Route>
 
         <Route exact path="/createAccount">
@@ -53,13 +62,12 @@ function App() {
 
         <Route exact path="/cartPage">
           <Elements stripe={stripePromise}>
-            <CartPage /> 
+            <CartPage />
           </Elements>
         </Route>
 
         <Route exact path="/orderPage">
-         <OrderPage />
-          
+          <OrderPage />
         </Route>
 
         <Route exact path="/userHomePage">
@@ -68,11 +76,6 @@ function App() {
 
         <Route exact path="/userInfo">
           <UserInfo />
-        </Route>
-
-
-<Route exact path="/orderPage">
-          {/* <OrderPage /> */}
         </Route>
 
         <Route exact path="/succsessPage">
